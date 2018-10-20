93d543e61830daf5eef0443180c9447702252c37
Samuel Kim <sams404link@gmail.com>
2018-10-20 16:26:28 -0700

    created basic portfolio app

---
+++ src/Components/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ProjectList from '../Data/ProjectList';
+
+const listProjects = () => {
+  console.log()
+}
+const Project = () => {
+  return(
+    <div>
+      <h1>Projects</h1>
+      
+    </div>
+  )
+}
+
+export default Project;
---
+++ src/Data/ProjectList.js
@@ -0,0 +1,22 @@
+const ProjectList = [
+  {
+    projectName:'Videorealm',
+    projectDesc:'Videorealm is a video sharing application that allows users to upload, watch and comment on videos.',
+    projectLink:'https://videorealm.herokuapp.com/',
+    projectTech:'Javascript, Es6, React, Redux, Node, Mongodb, Mongoose, Sengrid, Mocha, Chai, AWS S3, Mlab',
+  },
+  {
+    projectName:'Ironcodeman',
+    projectDesc:`Ironcodeman is a simple to use application which lists commands from various different frameworks including
+    Git, Heroku, Django, and so on. It also lists different programming concepts from languages like C and Javascript`,
+    projectLink:'www.ironcodeman.com/',
+    projectTech:'Javascript, Es6, React',
+  },
+  {
+    projectName:'Loanie',
+    projectDesc:`A mortgage management application which allows customers to track their mortgage application status and facilitates an 
+    otherwise long and often confusing process for obtaining mortgage loans.`,
+    projectLink:'https://loanie.pro/',
+    projectTech:'Javascript, Es6, React, Redux, Node, Mongodb, Mongoose, Sengrid, Mocha, Chai, AWS S3, Mlab',
+  },
+]
