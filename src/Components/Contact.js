4380e1f75a3f12718f8d7a75892b50f1421c1d20
Samuel Kim <sams404link@gmail.com>
2018-10-26 14:18:53 -0700

    added image for links

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -10,8 +10,9 @@ const Contact = () => {
         <p className="text-size">If you have any inquires, the best way to contact me would be through email or by phone.</p>
         <p className="text-size"><b>Email: </b>sam404kim@gmail.com</p>
         <p className="text-size"><b>Phone number: </b>(510) 589 - 8123</p>
-        <p className="text-size"><b>Github: </b> <a href="https://github.com/IllSmithDa">https://github.com/IllSmithDa</a></p>
-        <p className="text-size"><b>LinkedIn: </b> <a href="https://www.linkedin.com/in/samuel-kim-1336ab112/">https://www.linkedin.com/in/samuel-kim-1336ab112/</a></p>
+        <p className="text-size"><b>Other Links:</b></p>
+        <a href="https://github.com/IllSmithDa"><img alt="linkedin_link" src="https://i.imgur.com/Zts5Hul.png" className="image-size" /></a>
+        <a href="https://www.linkedin.com/in/samuel-kim-1336ab112/"><img alt="linkedin_link" src="https://i.imgur.com/VgNyC09.png" className="image-size" /></a>
       </div>
     </div>
   );
