60001c056fd78d10889e48eca07299d3d7d3b948
Samuel Kim <sams404link@gmail.com>
2018-10-26 16:29:15 -0700

    updated resume

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -8,7 +8,7 @@ const Resume = () =>{
       <br />
       <h1 className="section-title">Resume</h1>
       <br />
-      <iframe src="https://drive.google.com/file/d/1eX18C85QvmFlzcHFHPGsebPxKeKSfwHg/preview" width="100%" height="80%"></iframe>
+      <iframe src="https://drive.google.com/file/d/1E5DZHA5J-DTX22TCf7CfQETBl5YChIFU/preview" width="100%" height="80%"></iframe>
     </div>
   )
 }
