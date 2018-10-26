85d27aaa0d5a8bf8b53dbedc461b672fa92f0a00
Samuel Kim <sams404link@gmail.com>
2018-10-26 14:22:11 -0700

    edited contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -7,10 +7,10 @@ const Contact = () => {
       <h1 className="section-title">Contact</h1>
       <div className="center-contact">
       <br />
-        <p className="text-size">If you have any inquires, the best way to contact me would be through email or by phone.</p>
+        <p className="text-size"><b>If you have any inquires, the best way to contact me would be through email or by phone.</b></p>
         <p className="text-size"><b>Email: </b>sam404kim@gmail.com</p>
         <p className="text-size"><b>Phone number: </b>(510) 589 - 8123</p>
-        <p className="text-size"><b>Other Links:</b></p>
+        <p className="text-size"><b>Other ways to connect:</b></p>
         <a href="https://github.com/IllSmithDa"><img alt="linkedin_link" src="https://i.imgur.com/Zts5Hul.png" className="image-size" /></a>
         <a href="https://www.linkedin.com/in/samuel-kim-1336ab112/"><img alt="linkedin_link" src="https://i.imgur.com/VgNyC09.png" className="image-size" /></a>
       </div>
