b5041a0a790d6bee8d35c998f32b206de093ce71
Samuel Kim <sams404link@gmail.com>
2018-12-04 00:30:12 -0800

    centered contact page

--- src/Components/Errorpage.js
+++ src/Components/Errorpage.js
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 import "../CSS/PageLayout.css"
 const ErrorPage = () => {
   return (
-    <div className="page-container">
+    <div className="page-container contact-page-size">
       <Navbar />
         <h1 className="section-title"> 404: Page does not Exist</h1>
     </div>
