563111b64cb1c5547f908aef4af8a10da6e19228
Samuel Kim <sams404link@gmail.com>
2020-10-27 01:36:36 -0700

    added underline to nav

--- src/Components/Desktopnav/Desktopnav.js
+++ src/Components/Desktopnav/Desktopnav.js
@@ -7,16 +7,20 @@ class Desktopnav extends Component{
           currentID: "home-tab",
         }
       }
-    
+    componentDidMount() {
+        const { currentID } = this.state;
+        // remove underline of the previously clicked tab
+        document.getElementById(currentID).style.textDecoration = "underline";
+    }
     currentLink = (link, tab) => {
       // scroll into the selecte element
       document.getElementById(link).scrollIntoView({behavior: "smooth", block: "start"});
       // update current element 
       const { currentID } = this.state;
         // remove underline of the previously clicked tab
-  //    document.getElementById(currentID).style.textDecoration = "none";
+      document.getElementById(currentID).style.textDecoration = "none";
       // underline the new element
- //     document.getElementById(tab).style.textDecoration = "underline";
+     document.getElementById(tab).style.textDecoration = "underline";
       this.setState({ currentID: tab });
       // close tab on mobile
     }
@@ -30,7 +34,7 @@ class Desktopnav extends Component{
                             <span id="home-tab" onClick={()=>this.currentLink("Home-ID", "home-tab")}>Home</span>
                             <span id="about-tab" onClick={()=>this.currentLink("about-ID", "about-tab")}>About Me</span>
                             <span id="project-tab" onClick={()=>this.currentLink("project-ID", "project-tab")}>Projects</span>
-                            <span onClick={()=>this.currentLink("contact-ID", "contact-tab")} >Contact Me</span>
+                            <span id="contact-tab" onClick={()=>this.currentLink("contact-ID", "contact-tab")} >Contact Me</span>
                         </div>
                     </div>
                 </div>
