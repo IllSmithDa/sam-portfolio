9748aed714b7798baf15a42fc35fe7c4d7cc8a4a
Samuel Kim <sams404link@gmail.com>
2022-03-08 23:07:55 -0800

    added new files

--- src/Data/ProjectList.js
+++ src/Data/ProjectList.js
@@ -8,21 +8,46 @@ const projects = [
     imageSrc:`https://i.imgur.com/H41nUcH.png`
   },
   {
+    projectName:'Ironcodeman',
+    projectDesc:`Ironcodeman is a simple to use application which lists commands from various different frameworks including
+    Git, Heroku and Django.`,
+    projectLink:'http://www.ironcodeman.com/',
+    shortLink: 'ironcodeman.com',
+    projectTech:'Javascript, Es6, React',
+    imageSrc: `https://i.imgur.com/smDFnkU.png`,
+  },
+  {
+    projectName: 'Path of Anime',
+    projectDesc: `Path of anime is an experimental application that deals with all things anime including news, videos, and titles. 
+    The application is built on the Angular 2+ framework. `,
+    projectLink: 'https://pathofanime.surge.sh',
+    shortLink: 'pathofanime.com',
+    projectTech:'Angular 2, JavaScript, HTML, CSS',
+    imageSrc: 'https://i.imgur.com/6y5hX6k.png'
+  },
+  {
+    projectName: 'Infinite Firestorm',
+    projectDesc: 'Firestorm is a database for video games and includes many generations of both consoles and pc titles.',
+    projectLink:'https://firestorm.surge.sh',
+    shortLink: 'Firestorm.com',
+    projectTech: 'React.js, TypeScript, Styled-Components, JavaScript, HTML, CSS, Node.js, MongoDB', 
+    imageSrc: 'https://i.imgur.com/LBuAvWA.png',
+  },
+  {
     projectName:'Infinite News',
     projectDesc:'Infinite news is a news search application that allows users to search news using the Webit API.',
-    projectLink:'https://infinitenews.herokuapp.com/',
+    projectLink:'https://infinitenews.surge.sh',
     shortLink: 'infinitenews.herokuapp.com',
     projectTech:'Javascript, ES6, Angular, HTML, CSS, Bootstrap',
     imageSrc:`https://i.imgur.com/ccjtLkG.png`
   },
   {
-    projectName:'Ironcodeman',
-    projectDesc:`Ironcodeman is a simple to use application which lists commands from various different frameworks including
-    Git, Heroku, Django, and so on. It also lists different programming concepts from languages like C and JavaScript`,
-    projectLink:'http://www.ironcodeman.com/',
-    shortLink: 'ironcodeman.com',
-    projectTech:'Javascript, Es6, React',
-    imageSrc: `https://i.imgur.com/smDFnkU.png`,
+    projectName:'Infinite Free to Play',
+    projectDesc: 'Infinite Games a application that allows user to search free to play games that are currently out for PC.',
+    projectLink: 'https://infinite-games.surge.sh',
+    shortLink: 'infinite-games.com',
+    projectTech: 'React.js, TypeScript, Styled-Components, JavaScript, HTML, CSS',
+    imageSrc: 'https://i.imgur.com/0dbVqmf.png',
   },
   {
     projectName:'Infinite Softworks',
@@ -31,7 +56,12 @@ const projects = [
     shortLink: 'wonderland21.com/',
     projectTech:'Salesforce Community Cloud, LWC, Aura, Apex, JavaScript, Java, HTML, CSS',
     imageSrc:`https://i.imgur.com/sz83tE3.png`
-  },
+  }
+];
+export default projects;
+
+/*
+,
   {
     projectName:'Loanie',
     projectDesc:`Loanie is a mortgage management application which allows customers to track their mortgage application status and facilitates an 
@@ -41,5 +71,6 @@ const projects = [
     projectTech:'Javascript, Es6, React, Redux, Node, Mongodb, Mongoose, Sengrid, Mocha, Chai, AWS S3, MLab',
     imageSrc:`https://imgur.com/ccykjzr.png`
   },
-];
-export default projects;
+
+
+*/
